edbea6aef12920b81dfb73387e56f5528efa4373
bohdankonopolskyi <bohdan_konopolskyi@icloud.com>
2022-05-20 14:56:53 +0300

    lab3 completed

---
+++ ORPZ_Lab3_CreationalPatterns/ORPZ_Lab3_CreationalPatterns/PBFDK2Encryptor.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace ORPZ_Lab3_CreationalPatterns
+{
+    public class SHA256Encryptor
+    {
+        public SHA256Encryptor()
+        {
+        }
+    }
+}
---
+++ ORPZ_Lab3_CreationalPatterns/ORPZ_Lab3_CreationalPatterns/PBFDK2EncryptorCreator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace ORPZ_Lab3_CreationalPatterns
+{
+    public class SHA256EncryptorCreator
+    {
+        public SHA256EncryptorCreator()
+        {
+        }
+    }
+}
